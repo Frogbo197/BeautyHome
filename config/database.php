7ee5179c94d681f9116e60e5c55e074a9a6f0876
Frogbo197 <quynhnhu200404@gmail.com>
2026-05-28 13:06:16 +0700

    Force MySQL for Railway imported database

--- config/database.php
+++ config/database.php
@@ -17,7 +17,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => (env('DB_CONNECTION') === 'sqlite' && env('DB_DATABASE') === 'trolysuckhoe')
+        ? 'mysql'
+        : env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
